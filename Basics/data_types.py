cf2505595ab5d6e9d95e02c0b810584689647e45
mariamjahangeer <mariamjahangeer109@gmail.com>
2025-07-10 05:39:24 -0700

    https://github.com/mariamjahangeer/Basics_Python.git

---
+++ Basics/data_types.py
@@ -0,0 +1,26 @@
+# Python Data Types Demonstration
+
+# Basic Data Types
+int_val = 109                     # Integer
+float_val = 3.14                 # Float
+complex_val = 7 + 3j             # Complex
+bool_val = False                  # Boolean
+str_val = "Hello world!"       # String
+
+# Collection Data Types
+list_val = [1, 2, 3, "seven"]     # List (mutable, ordered)
+tuple_val = (5, 6, 7)            # Tuple (immutable, ordered)
+set_val = {1, 2, 3}              # Set (unique, unordered)
+dict_val = {"name": "Sara", "age": 25} # Dictionary (key-value)
+
+# Type Check
+print(type(int_val))        # <class 'int'>
+print(type(float_val))      # <class 'float'>
+print(type(complex_val))    # <class 'complex'>
+print(type(bool_val))       # <class 'bool'>
+print(type(str_val))        # <class 'str'>
+print(type(list_val))       # <class 'list'>
+print(type(tuple_val))      # <class 'tuple'>
+print(type(set_val))        # <class 'set'>
+print(type(dict_val))       # <class 'dict'>
+
